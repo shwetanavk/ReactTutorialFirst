adaaf7e9a39a59c353aefece891b01bafcf33d72
shwetanavkar <shwetanavkar18@gmail.com>
2021-11-30 21:37:07 +0530

    Use of Proper list

--- my-react-tutorial-demo/src/index.js
+++ my-react-tutorial-demo/src/index.js
@@ -14,32 +14,52 @@ import "./index.css";
 
 //Setup Vars
 
-const bookTitle = "Book Thief";
-const author = "Markus Zusak";
-const image = "https://m.media-amazon.com/images/I/81YPgi4vpDL._AC_UY218_.jpg";
+const books = [
+  {
+    img: "https://m.media-amazon.com/images/I/81YPgi4vpDL._AC_UY218_.jpg",
+    title: "Book Thief",
+    author: "Markus Zusak",
+  },
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/51Ua3JTN8iS._AC_SX184_.jpg",
+    title: "Big Shot Diary of a Wimpy Kid",
+    author: "Jeff KinneyJeff Kinney",
+  },
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/51f-G2GQwBL._AC_SX184_.jpg",
+    title: "Will",
+    author: "Will Smith",
+  },
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/51j8IiHZUtL._AC_SX184_.jpg",
+    title: "Atomic Habits: An Easy & Proven Way to Build Great Habit ",
+    author: "James ClearJames Clear",
+  },
+];
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book title={bookTitle} author={author} image={image}>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro
-          veritatis ipsum aut placeat, magni autem similique eveniet? Eos,
-          voluptatum!
-        </p>
-      </Book>
-      <Book />
+      {books.map((book) => {
+        console.log(book);
+        return <Book book={book} />;
+        {
+          /* return <Book img={book.img} title={book.title} author={book.author} />; */
+        }
+      })}
     </section>
   );
 }
 
 const Book = (props) => {
   console.log(props);
+  console.log(props.book);
+  const { img, title, author } = props.book;
   return (
     <article className="book">
-      <img src={props.image} alt="" />
-      <h1>{props.bookTitle}</h1>
-      <h4>-{props.author}</h4>
+      <img src={img} alt="" />
+      <h1>{title}</h1>
+      <h4>-{author}</h4>
 
       {props.children}
     </article>
