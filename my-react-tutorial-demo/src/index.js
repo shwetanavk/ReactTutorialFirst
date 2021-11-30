ade936ed310c8f8d8a780ea26ae4c3a1e3ca9c6b
shwetanavkar <shwetanavkar18@gmail.com>
2021-11-30 16:43:17 +0530

    Props

--- my-react-tutorial-demo/src/index.js
+++ my-react-tutorial-demo/src/index.js
@@ -11,26 +11,37 @@ import "./index.css";
 
 //Stateless functional component
 //Always return JSX
+
+//Setup Vars
+
+const bookTitle = "Book Thief";
+const author = "Markus Zusak";
+const image = "https://m.media-amazon.com/images/I/81YPgi4vpDL._AC_UY218_.jpg";
+
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book title={bookTitle} author={author} image={image}>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro
+          veritatis ipsum aut placeat, magni autem similique eveniet? Eos,
+          voluptatum!
+        </p>
+      </Book>
       <Book />
     </section>
   );
 }
 
-const Book = () => {
+const Book = (props) => {
+  console.log(props);
   return (
     <article className="book">
-      <img
-        src="https://m.media-amazon.com/images/I/81YPgi4vpDL._AC_UY218_.jpg"
-        alt=""
-      />
-      <h1>Book Thief</h1>;<h4>Author: Markus Zusak</h4>
+      <img src={props.image} alt="" />
+      <h1>{props.bookTitle}</h1>
+      <h4>-{props.author}</h4>
+
+      {props.children}
     </article>
   );
 };
