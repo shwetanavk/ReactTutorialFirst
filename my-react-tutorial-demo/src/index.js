afac121f4816bfa8918f3fee24e1628649f035ae
shwetanavkar <shwetanavkar18@gmail.com>
2021-11-30 15:06:12 +0530

    Creating components of book and adding CSS

--- my-react-tutorial-demo/src/index.js
+++ my-react-tutorial-demo/src/index.js
@@ -1,13 +1,56 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+//CSS
+import "./index.css";
 //Since index.js is the starting point of javascript in the
 //applicaition, we add this:
 
 //We capatilize the first letter of the function so react knows
 //this is a special type of function and it is a component
-function Greeting() {
-  return <h4>This is JSX and my first component</h4>;
+
+//Stateless functional component
+//Always return JSX
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
 }
 
-ReactDom.render(<Greeting />, document.getElementById("root"));
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <BookName />
+      <BookAuthor />
+    </article>
+  );
+};
+
+const Image = () => {
+  return (
+    <img
+      src="https://m.media-amazon.com/images/I/81YPgi4vpDL._AC_UY218_.jpg"
+      alt=""
+    />
+  );
+};
+
+const BookName = () => {
+  return <h1>Book Thief</h1>;
+};
+const BookAuthor = () => {
+  return (
+    <h4 style={{ color: "Blue", fontSize: "0.75rem", marginTop: "0.25rem" }}>
+      Author: Markus Zusak
+    </h4>
+  );
+};
+
+ReactDom.render(<BookList />, document.getElementById("root"));
