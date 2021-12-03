ca9b6cb9a5341435b56d55b04d882fdc840c7a4f
shwetanavkar <shwetanavkar18@gmail.com>
2021-12-03 15:58:06 +0530

    events

--- my-react-tutorial-demo/src/index.js
+++ my-react-tutorial-demo/src/index.js
@@ -16,21 +16,25 @@ import "./index.css";
 
 const books = [
   {
+    id: 1,
     img: "https://m.media-amazon.com/images/I/81YPgi4vpDL._AC_UY218_.jpg",
     title: "Book Thief",
     author: "Markus Zusak",
   },
   {
+    id: 2,
     img: "https://images-na.ssl-images-amazon.com/images/I/51Ua3JTN8iS._AC_SX184_.jpg",
     title: "Big Shot Diary of a Wimpy Kid",
     author: "Jeff KinneyJeff Kinney",
   },
   {
+    id: 3,
     img: "https://images-na.ssl-images-amazon.com/images/I/51f-G2GQwBL._AC_SX184_.jpg",
     title: "Will",
     author: "Will Smith",
   },
   {
+    id: 4,
     img: "https://images-na.ssl-images-amazon.com/images/I/51j8IiHZUtL._AC_SX184_.jpg",
     title: "Atomic Habits: An Easy & Proven Way to Build Great Habit ",
     author: "James ClearJames Clear",
@@ -41,8 +45,8 @@ function BookList() {
   return (
     <section className="booklist">
       {books.map((book) => {
-        console.log(book);
-        return <Book book={book} />;
+        return <Book key={book.id} book={book} />;
+        //return <Book key={book.id} {...book} ></Book>; //
         {
           /* return <Book img={book.img} title={book.title} author={book.author} />; */
         }
@@ -52,15 +56,31 @@ function BookList() {
 }
 
 const Book = (props) => {
-  console.log(props);
-  console.log(props.book);
+  //attribut, eventHandler
+  //onClick, onMouseOver
   const { img, title, author } = props.book;
+
+  const clickHandler = (e) => {
+    console.log(e);
+    console.log(e.target);
+    alert("adfa");
+  };
+
+  const complex = (a) => {
+    console.log(a);
+  };
+
   return (
     <article className="book">
       <img src={img} alt="" />
       <h1>{title}</h1>
       <h4>-{author}</h4>
-
+      <button type="button" onClick={clickHandler}>
+        Click ME!
+      </button>
+      <button type="button" onClick={() => complex(author)}>
+        ComplexWithArgument
+      </button>
       {props.children}
     </article>
   );
